659e0d9b2477784a5941138e7796740d60d42b9c
Your Name <you@example.com>
2020-06-22 14:48:49 -0400

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js v1.0\n');
+  res.send('Hello Node.js v2.0\n');
 });
 
 app.listen(PORT, HOST);
