1504eb6f6ea5e04e26182ee4493da0828850adce
user <user@this.com>
2020-06-22 13:29:49 -0400

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT=9090;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Node.js v1.0\n');
+});
+
+app.listen(PORT, HOST);
+
